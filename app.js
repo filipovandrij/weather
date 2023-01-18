ec321c0b56c0f4c682ef5e502f40f27df23425fa
Filipov <andrijfilipov73@gmail.com>
2023-01-18 22:07:32 +0200

    test a long study JS

---
+++ app.js
@@ -0,0 +1,44 @@
+const city = prompt("Writte your city. For example:", "KYIV");
+fetch(
+  `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&APPID=5d066958a60d315387d9492393935c19`
+)
+  .then((res) => res.json())
+  .then((data) => {
+    console.log(data);
+
+    let valueOfTemp = data.main.temp;
+    let valueVisibleTemp = document.querySelector(".temp");
+    valueVisibleTemp.innerHTML = valueOfTemp;
+
+    let valueOfDescription = data.weather[0].description;
+    let valueVisibleDescription = document.querySelector(".description");
+    valueVisibleDescription.innerHTML = valueOfDescription;
+
+    let valueOfPressure = data.main.pressure;
+    let valueVisiblePressure = document.querySelector(".pressure");
+    valueVisiblePressure.innerHTML = valueOfPressure;
+
+    let valueOfHumidity = data.main.humidity;
+    let valueVisibleHumidity = document.querySelector(".humidity");
+    valueVisibleHumidity.innerHTML = valueOfHumidity;
+
+    let valueOfSpeed = data.wind.speed;
+    let valueVisibleSpeed = document.querySelector(".speed");
+    valueVisibleSpeed.innerHTML = valueOfSpeed;
+
+    let valueOfDegree = data.wind.deg;
+    let valueVisibleDegree = document.querySelector(".deg");
+    valueVisibleDegree.innerHTML = valueOfDegree;
+  });
+
+let showCity = document.querySelector(".city");
+showCity.innerHTML = city;
+
+//https://openweathermap.org/img/w/10d.png
+//   temp - температура
+// pressure - тиск
+// description - опис
+// humidity - вологість
+// speed - швидкість вітру
+// deg - напрям в градусах
+// icon - іконка, де 10d код іконки
